2d40c0e9ddb608672d9fa1864c00cbf0136309d2
Kaijun Feng <kfeng19@proton.me>
2023-12-22 15:36:00 -0800

    reuse click option

--- src/butler/ui.py
+++ src/butler/ui.py
@@ -1,5 +1,6 @@
 import click
-from butler.authentication import initialize
+
+from butler.authentication import initialize, authenticate
 
 
 @click.group()
@@ -7,16 +8,38 @@ def cli():
     pass
 
 
+def str2bytes(ctx, param, value):
+    if isinstance(value, str):
+        return str.encode(value)
+    else:
+        return value
+
+
+def pw_option(func):
+    return click.option(
+        "--password",
+        hide_input=True,
+        prompt="Your root password",
+        callback=str2bytes,
+        help="This is used for secret encryption.",
+    )(func)
+
+
 @click.command()
-@click.option(
-    "--password",
-    hide_input=True,
-    prompt="Your root password",
-    help="This will be used for encrypting your secrets.",
-)
+@pw_option
 def init(password):
     """Initialize a root password for authentication"""
-    initialize(str.encode(password))
+    initialize(password)
+
+
+@click.command()
+@pw_option
+def ls(password):
+    if authenticate(password):
+        print("Hooray!")
+    else:
+        print("Wrong password :(")
 
 
 cli.add_command(init)
+cli.add_command(ls)
